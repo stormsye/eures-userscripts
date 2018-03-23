c17b5c00587efe06dd1f05fcf6c03609829449f2
Yentl Storms <yentlstorms>
2018-03-23 16:53:03 +0100

    temp

--- sidebar/sidebar.js
+++ sidebar/sidebar.js
@@ -1,5 +1,11 @@
-// @require      http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
-// @grant        GM_addStyle
+// The following must be specified on the importing script:
+// @require		http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+// @resource	fontAwesomeCss https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css
+// @resource	sidebarCss https://raw.githubusercontent.com/stormsye/eures-userscripts/0.2.x/sidebar/sidebar.css
+// @grant		GM_addStyle
+// @grant		GM_getResourceText
+
+GM_addStyle(GM_getResourceText(sidebarCss));
 
 createSidebar = function(toggleKey = 120, defaultCollapsed = true) {
     const STATE_OPEN = "expanded";
@@ -34,47 +40,3 @@ createSidebar = function(toggleKey = 120, defaultCollapsed = true) {
         toggleSidebar: toggleSidebar
     };
 };
-
-GM_addStyle(`
-    #fullSidebar {
-        position:                 fixed;
-        left:                     0;
-        top:                      50%;
-        z-index:                  5000;
-        opacity:                  0.9;
-        display:                  inline-block;
-    }
-
-    #fullSidebar.collapsed {
-        transform:                translate(-80px, -50%);
-        transition:               transform 1s;
-    }
-
-    #fullSidebar.expanded {
-        transform:                translate(0px, -50%);
-        transition:               transform 1s;
-    }
-
-    #fullSidebarContent {
-        display:                  inline-block;
-        vertical-align:           middle;
-        width:                    80px;
-        min-height:               300px;
-        max-height:               600px;
-        background:               black;
-        opacity:                  0.85;
-        float:                    left;
-        padding:                  3px;
-    }
-
-    #fullSidebarCollapser {
-        display:                  inline-block;
-        vertical-align:           middle;
-        width:                    10px;
-        border-top:               60px solid transparent;
-        border-left:              15px solid black;
-        border-bottom:            60px solid transparent;
-        opacity:                  0.9;
-        cursor:                   pointer;
-    }
-`);
